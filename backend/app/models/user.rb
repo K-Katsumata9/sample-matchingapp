fd8fecdfe3214140f13b0c9f2914c849d5826189
K-Katsumata9 <dowbler@icloud.com>
2024-05-21 21:22:32 +0900

    create sign_up%sign_in form

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   validates :name, presence: true
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+	enum gender: { man: 0, woman: 1 }
 end
