b7440e67983f969598d286953af168a79b796efb
K-Katsumata9 <dowbler@icloud.com>
2024-05-25 00:28:21 +0900

    add preview function

--- backend/app/javascript/profile/profile_image_upload.js
+++ backend/app/javascript/profile/profile_image_upload.js
@@ -1,24 +1,21 @@
-document.getElementById("profile_image_upload").addEventListener("change", function(e) {
-	if (e.target.files.length) {
-		let reader = new FileReader();
-		reader.onload = function(e) {
-			// Remove the 'hidden' class from elements
-			document.querySelectorAll('.hidden').forEach(function(element) {
-				element.classList.remove('hidden');
-			});
-			// Remove the 'profile-default-img' class from elements
-			document.querySelectorAll('.profile-default-img').forEach(function(element) {
-				element.classList.remove('profile-default-img');
-			});
-			// Remove the element with id 'profile-img' if it exists
-			let profileImg = document.getElementById('profile-img');
-			if (profileImg) {
-				profileImg.remove();
-			}
-			// Set the src attribute of the element with id 'profile-img-prev' to the image data
-			document.getElementById('profile-img-prev').setAttribute('src', e.target.result);
-		};
-		reader.readAsDataURL(e.target.files[0]);
-	}
+document.addEventListener("change", function(e) {
+  if (e.target.id === "profile_image_upload") {
+    if (e.target.files.length) {
+      let reader = new FileReader();
+      reader.onload = function(e) {
+        document.querySelectorAll('.hidden').forEach(function(element) {
+          element.classList.remove('hidden');
+        });
+        document.querySelectorAll('.profile-default-img').forEach(function(element) {
+          element.classList.remove('profile-default-img');
+        });
+        let profileImg = document.getElementById('profile-img');
+        if (profileImg) {
+          profileImg.remove();
+        }
+        document.getElementById('profile-img-prev').setAttribute('src', e.target.result);
+      };
+      reader.readAsDataURL(e.target.files[0]);
+    }
+  }
 });
-
